2bd79d0943e374d41b692bb48636662df3261a3d
PeterTYLiu <peter.ty.liu@gmail.com>
2023-06-14 04:01:47 -0400

    add icons and scripts for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "icon-192.png", "icon-512.png"],
+
+      manifest: {
+        name: "RuneSweeper",
+        short_name: "RuneSweeper",
+        description: "RuneScape-themed Minesweeper PWA",
+        theme_color: "#5C4016",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/",
+});
